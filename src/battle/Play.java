0fb62a7c08953506188b625e5e1dd0cfb3d38f3e
Al-Rou <57326693+Al-Rou@users.noreply.github.com>
2019-11-03 20:41:08 +0100

    Change in message again!

--- src/battle/Play.java
+++ src/battle/Play.java
@@ -94,7 +94,7 @@ public class Play
         else if (monster.getLife()<=0)
         {
             System.out.println("Congratulations!\n"+
-                    "You killed the monster, bro!");
+                    "You killed the monster, brother");
             System.out.print("Your Life: ");
             System.out.println(spieler.getLife());
             System.out.print("Your Mana: ");
